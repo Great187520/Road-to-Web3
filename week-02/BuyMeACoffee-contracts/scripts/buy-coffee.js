57e714fa39a332080b11aaecc63e5d67ed3d8c88
Eleke Great Chima <59459106+Great187520@users.noreply.github.com>
2022-06-06 23:45:31 +0100

    Add files via upload

---
+++ week-02/BuyMeACoffee-contracts/scripts/buy-coffee.js
@@ -0,0 +1,80 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+// Returns the Ethere balance of a given address.
+async function getBalance(address) {
+  const balanceBigInt = await hre.waffle.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+// Logs the Ether balances for a list of addresses.
+async function printBalances(addresses) {
+  let idx = 0;
+  for (const address of addresses) {
+    console.log(`Address ${idx} balance: `, await getBalance(address));
+    idx++;
+  }
+}
+
+// Logs the memos stored 0n-chain from coffee purchases.
+async function printMemos(memos) {
+  for (const memo of memos){
+    const timestamp = memo.timestamp;
+    const tipper = memo.name;
+    const tipperAddress = memo.from;
+    const message = memo.message;
+    console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`);
+  }
+}
+
+async function main() {
+  // Get example accounts.
+  const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
+
+  // Get the contract to deploy & deploy.
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
+  await buyMeACoffee.deployed();
+  console.log("BuyMeACoffee deployed to ", buyMeACoffee.address);
+
+  // check balances before the coffee purchase.
+  const address = [owner.address, tipper.address, buyMeACoffee.address];
+  console.log("== start ==");
+  await printBalances(addresses);
+
+  // Buy the owner a few coffees.
+  const tip = {value: hre.ethers.utils.parseEther("1")};
+  await buyMeACoffee.connect(tipper).buyCoffee("Carolina", "You're the best!", tip);
+  await buyMeACoffee.connect(tipper2).buyCoffee("Vitto", "Amazing Teacher :)", tip);
+  await buyMeACoffee.connect(tipper3).buyCoffee("kay", "I love my proof of Knowledge NFt", tip);
+
+
+  // Check balances after coffee purchase.
+  console.log("== bought coffee ==");
+  await printBalances(addresses);
+
+  // Withdra funds.
+  await buyMeACoffee.connect(owner).withdrawTips();
+
+  // Check balance after withdraws.
+  console.log("== withdrawTips ==");
+  await printBalances(addresses);
+
+  // Read all the memos left for the owner.
+  console.log("== memos ==");
+  const memos = await buyMeACoffee.getMemos();
+  printMemos(memos);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
